290aa5bcc5d0ac47b35ca39e342309b900854595
Alexander Andrenko <andrenkom@gmail.com>
2020-03-29 16:06:51 +0700

    Modificated the implementation for task

--- Compuattional mathematics/Newtone method.cpp
+++ Compuattional mathematics/Newtone method.cpp
@@ -4,21 +4,21 @@
 
 using namespace std;
 
-double valueOfFunction(double x)
+double valueOfFunction(double x)//функция расчёта значения функции в точке
 {
 	double result = tan(1.5773 * x) - 2.3041 * x;
 
 	return result;
 }
 
-double calcDerivative(double x)
+double calcDerivative(double x)//функция расчёта значения первой производной
 {
 	double valueDerivative = 1.5773 * tan(pow(1.5773 * x, 2)) - 0.7268;
 
 	return valueDerivative;
 }
 
-double calcSecondDerivative(double x)
+double calcSecondDerivative(double x)//функция расчёта значения второй производной
 {
 	double valueDerivative = 3.1546 * tan(1.5773 * x) * (1.5773 * tan(pow(1.5773 * x, 2)) + 1.5773);
 
@@ -29,39 +29,34 @@ int main()
 {
 	setlocale(LC_ALL, "Russian");
 
-	double x0; //НАчальное приближение корня
+	double x0; //Начальное приближение корня
 	double epsilon;//точность вычислений    
-	double x1 = 1;
-	double savePreX = 0;
-
-
+	double x1;//Значение следующего приближения
+	double delta;//Разность значений между предыдущим и следующим корнями
 
 	cout << "Введите начальное приближение: ";
 	cin >> x0;
 	cout << "Введите точность вычислений: ";
 	cin >> epsilon;
 
-	if (abs((valueOfFunction(x0) * calcSecondDerivative(x0)) / pow(calcDerivative(x0), 2)) < 1)
+	if (fabs((valueOfFunction(x0) * calcSecondDerivative(x0)) / pow(calcDerivative(x0), 2)) < 1)//Проверка условия сходимости
 	{
+		//Цикл поиска корня
 		do
 		{
-			x1 = x0 - (valueOfFunction(x0) / calcDerivative(x0));
+			x1 = x0 - (valueOfFunction(x0) / calcDerivative(x0));//Расчёт следующего приближения
 			cout << "х0: " << x0 << "\t" << "x1:" << x1 << endl;
-			savePreX = x0;
+			delta = x1 - x0;//Разница между следующим и предыдущим корнями
 			x0 = x1;
 
-		} while (abs(x1 - savePreX) > epsilon);
+		} while (fabs(delta) > epsilon);
+
+		cout << "Корень уравнения: " << x1 << endl;
+		cout << "Значение функции в данной точке: " << tan(1.5773 * x1) - 2.3041 * x1 << endl;
 	}
 	else
 	{
 		cout << "К сожалению приближенное значение не удовлетворяет условию сходимости!\n";
-	}
-
-
-	if (abs(x1 - savePreX) < epsilon)
-	{
-		cout << "Корень уравнения: " << x1 << endl;
-		cout << "Значение функции в данной точке: " << tan(1.5773 * x1) - 2.3041 * x1 << endl;
 	}
 
 	system("pause");
