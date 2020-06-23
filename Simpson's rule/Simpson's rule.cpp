62746b615abf56632d1fc1f8b78258a6491be934
Alexander Andrenko <andrenkom@gmail.com>
2020-06-23 21:44:48 +0700

    I Started project Simpson's rule implementing computation of the integral

---
+++ Simpson's rule/Simpson's rule.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <clocale>
+#include <cmath>
+
+using namespace std;
+
+/*Программа расчёта определённого интеграла методом Симпсона*/
+
+typedef double value;//Тип данных используемый для результатов вычислений
+
+value ValueOfIntegrand(value x)//Вычисление значения подынтегральной функции в заданной точке
+{
+    return ((x / 2) * log((x * x) / 2));
+}
+
+value SimpsonsRule(value lowerIntervalValue, value upperIntervalValue, int quantityOfIntervals)//Параметры: Нижнее значение интервала, Верхнее значение интервала, Количество интервалов
+{
+    value step = (upperIntervalValue - lowerIntervalValue) / quantityOfIntervals;//Вычисление длинны шага разбиения
+
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    
+
+    return 0;
+}
