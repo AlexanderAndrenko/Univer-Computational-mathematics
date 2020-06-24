09f257dec74021768b848b45d063c6960bfd0ff1
Alexander Andrenko <andrenkom@gmail.com>
2020-06-24 21:08:08 +0700

    Created gaussian project

---
+++ Gaussian quadrature/Gaussian quadrature.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+#include <clocale>
+
+using namespace std;
+
+/*Программа расчёта определённого интеграла методом Гаусса*/
+
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	system("pause");
+
+	return 0;
+}
